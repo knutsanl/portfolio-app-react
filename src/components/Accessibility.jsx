50efcf7b842ce185017c750b74af9eb8f5696d07
knutsanl <knutsanl@stud.ntnu.no>
2022-05-30 21:40:11 +0200

    more stuff
    stuff

---
+++ src/components/Accessibility.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+
+
+const Accessibility = () => {
+    return (
+        <div name='accessibility' className='w-full h-screen bg-[#BBDAD0]'>
+             <div className='flex flex-col justify-center items-center w-full h-full'>
+        <div className='max-w-[1000px] w-full grid grid-cols-2 gap-8'>
+          <div className='sm:text-right pb-8 pl-4'>
+            <p className='text-4xl font-bold inline border-b-4 border-[#6390E4]'>
+              solar System
+            </p>
+          </div>
+          <div></div>
+          </div>
+
+       
+        </div>
+        </div>
+        );
+    };
+
+export default Accessibility;
