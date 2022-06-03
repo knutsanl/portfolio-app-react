1409baa1b1cf0e7a6608311ca25b8630f307124d
knutsanl <knutsanl@stud.ntnu.no>
2022-06-03 17:24:15 +0200

    fixed bug
    bug mobile fix

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div name='contact' className='w-full h-screen bg-[#BBDAD0] flex justify-center items-center p-4'>
+    <div name='contact' className='w-full  bg-[#BBDAD0] flex justify-center items-center p-4'>
       {/*  <form method='POST' action="https://getform.io/f/a699a1b2-f225-434e-b317-1fbbde8e006c" className='flex flex-col max-w-[600px] w-full'>
             <div className='pb-8'>
                 <p className='text-4xl font-bold inline border-b-4 border-[#234099] '>Contact</p>
@@ -15,7 +15,7 @@ const Contact = () => {
         </form> */}
 
 <div className='flex flex-col justify-center items-center w-4/5 h-full'>
-            <div className='sm:text-right text-4xl font-bold'>
+            <div className='sm:text-right text-4xl font-bold bg-[#BBDAD0]'>
               <p>About the Portfolio</p>
             </div>
             <div>
